93ebfed1dd06d049bfe68494fb2398d064f9222d
rami628 <ramiabdullah0@gmail.com>
2023-07-22 18:48:23 +0400

    added sort and filter buttons

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './medicine';
 import pic from './medicine1.jpg';
 import './CylinderPage.css';
@@ -6,6 +6,9 @@ import pic2 from './medicine2.jpg';
 import { Link } from 'react-router-dom';
 
 const CylinderPage = ({ searchQuery }) => {
+    const [filter, setFilter] = useState(''); // State for the filter option
+    const [sort, setSort] = useState(''); // State for the sort option
+
     const products = [
         {
             image: pic,
@@ -19,12 +22,7 @@ const CylinderPage = ({ searchQuery }) => {
             description: '110 softgels',
             price: '90$',
         },
-        {
-            image: pic2,
-            title: 'E-400',
-            description: '110 softgels',
-            price: '90$',
-        },
+
         {
             image: pic,
             title: '5-HTP',
@@ -55,24 +53,81 @@ const CylinderPage = ({ searchQuery }) => {
             description: '50mg',
             price: '100$',
         },
+        {
+            image: pic2,
+            title: 'E-400',
+            description: '110 softgels',
+            price: '90$',
+        },
+        {
+            image: pic,
+            title: '5-HTP',
+            description: '50mg',
+            price: '100$',
+        },
+        {
+            image: pic2,
+            title: 'E-400',
+            description: '110 softgels',
+            price: '90$',
+        },
+
         // Add more product objects as needed
     ];
 
-    // Filter products based on search query
-    const filteredProducts = searchQuery
-        ? products.filter((product) =>
+    // Filter products based on search query, filter, and sort options
+    const filteredProducts = products
+        .filter((product) =>
             product.title.toLowerCase().includes(searchQuery.toLowerCase())
         )
-        : products;
+        .filter((product) => {
+            if (!filter) return true;
+            if (filter === 'mg') return product.description.toLowerCase().includes('mg');
+            if (filter === 'softgels') return product.description.toLowerCase().includes('softgels');
+            return true;
+        })
+        .sort((a, b) => {
+            if (sort === 'asc') {
+                return parseFloat(a.price) - parseFloat(b.price);
+            } else if (sort === 'desc') {
+                return parseFloat(b.price) - parseFloat(a.price);
+            } else {
+                return 0;
+            }
+        });
+
+    // Function to handle filter change
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value);
+    };
+
+    // Function to handle sort change
+    const handleSortChange = (event) => {
+        setSort(event.target.value);
+    };
 
     return (
         <div className="container-xyz">
             <h1>Our products</h1>
+            <div className="filter-container">
+                {/* Filter input */}
+                <select value={filter} className='filter-list' onChange={handleFilterChange}>
+                    <option value="">All</option>
+                    <option value="mg">mg</option>
+                    <option value="softgels">softgels</option>
+                </select>
+                {/* Sort options */}
+                <select value={sort} className='sort-list' onChange={handleSortChange}>
+                    <option value="">Default</option>
+                    <option value="asc">Price: Low to High</option>
+                    <option value="desc">Price: High to Low</option>
+                </select>
+            </div>
             <div className="card-container-xyz">
                 {filteredProducts.length > 0 ? (
                     filteredProducts.map((product, index) => (
                         <Link to="/product" className="card-xyz clicked-xyz" style={{ color: 'black', textDecoration: 'none' }} key={index}>
-                            <div >
+                            <div>
                                 <Card
                                     image={product.image}
                                     title={product.title}
